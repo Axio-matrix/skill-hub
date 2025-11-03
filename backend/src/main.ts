bf5e06795d75819e66ecb681f04bda683a79c15e
axio matrix <a.khedari@esi-sba.dz>
2025-11-03 21:06:00 +0100

    host v4

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,18 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as fs from 'fs';
-import * as path from 'path';
 
 async function bootstrap() {
-  const httpsOptions = {
-    key: fs.readFileSync(path.join(__dirname, '../../cert/key.pem')),
-    cert: fs.readFileSync(path.join(__dirname, '../../cert/cert.pem')),
-  };
-
-  const app = await NestFactory.create(AppModule, {
-    httpsOptions,
-  });
-
+  const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: true,
     credentials: true,
